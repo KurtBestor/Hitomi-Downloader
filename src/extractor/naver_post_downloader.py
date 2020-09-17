dcc5aca73bb6041804a837f0d29da8f18decb1e3
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-18 07:45:07 +0900

    refactor(NaverPost): func

--- src/extractor/naver_post_downloader.py
+++ src/extractor/naver_post_downloader.py
@@ -26,47 +26,57 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+import json
+from typing import Generator
+from urllib.parse import urlparse
 
 import clf2
-from utils import Session, Downloader, Soup
+from utils import Session, Downloader, Soup, clean_title
 
 
 @Downloader.register
 class DownloaderNaverPost(Downloader):
-    type = "post"  # 타입
+    type = "naver_post"  # 타입
+    URLS = ["m.post.naver.com", "post.naver.com"]
 
     def init(self):
-        pass
+        self.url = self.url.replace("naver_post_", "")
+        self.parsed_url = urlparse(self.url)
+        self.soup = get_soup(self.url)
 
     def id(self):
         pass
 
+    @property
+    def name(self):
+        return get_title(self.soup)
+
     def read(self):
-        self.title = "test"
-        url = self.url.replace("post_", "")
-        html = get_soup(url)
-        img_urls = get_img_links(html)
-        self.title = "aa"
-        for img_url in img_urls:
-            self.urls.append(img_url)
+        if self.parsed_url.path.startswith("/viewer"):
+            self.title = self.name
+            data_linkdatas = get_img_data_linkdatas(self.soup)
 
 
-def get_img_links(soup) -> list:
-    imgs_element = soup.find_all(
-        "img", id=lambda value: value and value.startswith("SEDOC"), src=True
-    )  # src와 SEDOC으로 시작하는거만 가져옴
-
-    img_links = [img_element["src"] for img_element in imgs_element]  # 링크 뽑아오기
-    return img_links
-
-
-def get_posts(url) -> list:
-    pass  # TODO: 포스트 링크들 가져오기
-
-
-# https://github.com/KurtBestor/Hitomi-Downloader/blob/master/src/extractor/manatoki_downloader.py#L84
-def get_soup(url):
+# https://github.com/KurtBestor/Hitomi-Downloader/blob/master/src/extractor/manatoki_downloader.py#L84 참고
+def get_soup(url: str):
     session = Session()
     res = clf2.solve(url, session=session)
     soup = Soup(res["html"])
     return soup
+
+
+# HTML5 data-* 속성이 사용됨.
+def get_img_data_linkdatas(soup) -> list:
+    a_elements = soup.find_all("a", {"data-linktype": "img"})  # 링크 타입이 img인것만 전부 찾음
+    return [a_element["data-linkdata"] for a_element in a_elements]  # 링크 데이터 리스트
+
+
+def generator_img_src_list(linkdatas: list) -> Generator:
+    for linkdata in linkdatas:
+        img_json = json.load(linkdata)  # a문제
+
+
+def get_title(soup):
+    title = soup.find("h3", class_="se_textarea")  # 포스트제목
+    author = soup.find("span", class_="se_author")  # 작성자
+    return clean_title(f"{title.text} ({author.text})")  # 무난하게 븉임
