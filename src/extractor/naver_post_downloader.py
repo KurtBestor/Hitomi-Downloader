23d36528d4b04ac0d0d8d46a4e6683d9b83b95f7
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-18 08:00:17 +0000

    refactor(NaverPost): using class

--- src/extractor/naver_post_downloader.py
+++ src/extractor/naver_post_downloader.py
@@ -28,7 +28,9 @@ SOFTWARE.
 """
 from distutils.util import strtobool
 from typing import Generator
-from urllib.parse import ParseResult, urlparse
+from urllib.parse import urlparse, parse_qs
+
+import requests
 
 import clf2
 from utils import Session, Downloader, Soup, clean_title
@@ -47,22 +49,22 @@ class DownloaderNaverPost(Downloader):
     def id(self):
         pass
 
-    @property
-    def name(self):
-        return get_title(self.soup)
-
     def read(self):
+        title = Title(self.soup)
         if self.parsed_url.path.startswith("/viewer"):
-            self.title = self.name
+            self.title = title.get_title()
             data_linkdatas = get_img_data_linkdatas(self.soup)
+
         elif self.parsed_url.path.startswith("/my.nhn"):
-            total = get_total_post(self.soup)
+            self.title = title.get_profile_title()
+
         elif self.parsed_url.path.startswith("/my/series"):
-            total = get_series_total_post(self.soup)
+            self.title = title.get_series_title()
+
         else:
             return self.Invalid("유효하지 않은 링크")
 
-        for img_link in generator_img_src(data_linkdatas):
+        for img_link in img_src_generator(data_linkdatas):
             self.urls.append(img_link)
 
 
@@ -80,44 +82,69 @@ def get_img_data_linkdatas(soup: Soup) -> list:
     return [a_element["data-linkdata"] for a_element in a_elements]  # 링크 데이터 리스트
 
 
-def generator_img_src(linkdatas: list) -> Generator:
+def img_src_generator(linkdatas: list) -> Generator:
     for linkdata in linkdatas:
         if not strtobool(linkdata["linkUse"]):  # 링크 없는것만
             yield linkdata["src"]  # 제네레이터
 
 
-# 0: 팔로워 1: 팔로잉 2: 포스트 3: 좋아요한글
-def get_total_post(soup: Soup):
-    profile_info = soup.find("div", class_="expert_num_info")  # 프로필 정보
-    total_post_element = profile_info.find_all("li", class_="inner")[2]
-    return total_post_element.find("span", class_="num").text  # 총몇개인지만 리턴
+class UrlGenerator:
+    def __init__(self, parsed_url, total_count):
+        self.parsed_url = parsed_url
+        self.total_count = total_count
+
+    def all_series_post_url_generator(self):
+        query = parse_qs(self.parsed_url.query)
+        for i in range(self.total_count):
+            new_url_query = f"?memberNo={query['memberNo'][0]}&seriesNo={query['seriesNo'][0]}&fromNo={i + 1}&totalCount={self.total_count}"  # 쿼리를 만듭시다
+            url = f"https://{self.parsed_url.netloc}/my/series/detail/more.nhn{new_url_query}"
+            yield url
+
+    def all_post_query_url_generator(self):
+        query = parse_qs(self.parsed_url.query)
+        for i in range(self.total_count):
+            new_url_query = f"?{query['memberNo'][0]}&fromNo={i + 1}&totalCount={self.total_count}"  # 쿼리를 만듭시다
+            url = f"https://{self.parsed_url.netloc}/async{self.parsed_url.path}{new_url_query}"
+            yield url
 
 
-# 0: 포스트 1: 팔로워
-def get_series_total_post(soup: Soup):
-    series_info = soup.find("div", class_="series_follow_area")  # 시리즈 정보
-    total_post_element = series_info.find_all("a")[0]
-    return total_post_element.find("em").text  # 총몇개인지만 리턴
+class Total:
+    def __init__(self, soup: Soup):
+        self.soup = soup
+
+    # 0: 팔로워 1: 팔로잉 2: 포스트 3: 좋아요한글
+    def get_total_post(self):
+        profile_info = self.soup.find("div", class_="expert_num_info")  # 프로필 정보
+        total_post_element = profile_info.find_all("li", class_="inner")[2]
+        return total_post_element.find("span", class_="num").text  # 총몇개인지만 리턴
+
+    # 0: 포스트 1: 팔로워
+    def get_series_total_post(self):
+        series_info = self.soup.find("div", class_="series_follow_area")  # 시리즈 정보
+        total_post_element = series_info.find_all("a")[0]
+        return total_post_element.find("em").text  # 총몇개인지만 리턴
 
 
-def get_profile_title(soup: Soup):
-    profile_name = soup.find("p", class_="nick_name").find(
-        "span", class_="name"
-    )  # 프로필 닉네임
-    return clean_title(profile_name.text)  # 닉네임으로만
+class Title:
+    def __init__(self, soup: Soup):
+        self.soup = soup
 
+    def get_profile_title(self):
+        profile_name = self.soup.find("p", class_="nick_name").find(
+            "span", class_="name"
+        )  # 프로필 닉네임
+        return clean_title(profile_name.text)  # 닉네임으로만
 
-def get_series_title(soup: Soup) -> clean_title:
-    series_name = soup.find("h2", class_="tit_series").find(
-        "span", class_="ell"
-    )  # 시리즈 제목
-    author = soup.find("div", class_="series_author_wrap").find(
-        "strong", class_="ell1"
-    )  # 작성자
-    return clean_title(f"{series_name.text} ({author.text})")  # 무난하게 붙임
+    def get_series_title(self) -> clean_title:
+        series_name = self.soup.find("h2", class_="tit_series").find(
+            "span", class_="ell"
+        )  # 시리즈 제목
+        author = self.soup.find("div", class_="series_author_wrap").find(
+            "strong", class_="ell1"
+        )  # 작성자
+        return clean_title(f"{series_name.text} ({author.text})")  # 무난하게 붙임
 
-
-def get_title(soup: Soup) -> clean_title:
-    title = soup.find("h3", class_="se_textarea")  # 포스트 제목
-    author = soup.find("span", class_="se_author")  # 작성자
-    return clean_title(f"{title.text} ({author.text})")  # 무난하게 붙임
+    def get_title(self) -> clean_title:
+        title = self.soup.find("h3", class_="se_textarea")  # 포스트 제목
+        author = self.soup.find("span", class_="se_author")  # 작성자
+        return clean_title(f"{title.text} ({author.text})")  # 무난하게 붙임
