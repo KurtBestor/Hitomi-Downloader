1dbf448901f04173bef91e2059c93e709b7cd6b6
bog_4t <97702184+bog4t@users.noreply.github.com>
2022-01-17 21:47:02 -0500

    wayback_machine_downloader.py: basic twitter optimizations

--- src/extractor/wayback_machine_downloader.py
+++ src/extractor/wayback_machine_downloader.py
@@ -4,10 +4,11 @@
 # author: bog_4t
 
 import downloader
-from utils import Downloader, Soup, Session, get_print
+from utils import Downloader, Session, get_print, update_url_query
 from ratelimit import limits, sleep_and_retry
 import ree as re
 import json
+import datetime
 import errors
 from error_printer import print_error
 
@@ -19,9 +20,13 @@ class Downloader_wayback_machine(Downloader):
     icon = 'https://archive.org/services/img/publicsafetycode'
     display_name = 'Wayback Machine'
 
+    def init(self):
+        self.session = Session()
+
     def read(self):
-        self.urls.extend(get_imgs(self.url, Session(), cw=self.cw))
-        self.title = 'placeholder'
+        query = WaybackMachineAPI.get_query(self.url)
+        self.urls.extend(get_imgs(query, self.session, cw=self.cw))
+        self.title = '{:%Y%m%d%H%M%S}'.format(datetime.datetime.now())
 
 
 class WaybackMachineAPI(object):
@@ -32,6 +37,8 @@ class WaybackMachineAPI(object):
         self.params = {
             'status code': '200',
             'filter': 'mimetype:text/html',
+            'fastLatest': 'true',
+            'collapse': 'urlkey',
             'output': 'json'
         }
 
@@ -40,10 +47,12 @@ class WaybackMachineAPI(object):
     def call(self, url_for_query):
         params = self.params
         params['url'] = url_for_query
-        response = self.session.get(self.url_api, params=params)
-        if response.status_code != 200:
-            response.raise_for_status()
-        return response.json()
+        url = update_url_query(self.url_api, params)
+        return downloader.read_json(url, session=self.session)
+
+    @classmethod
+    def get_query(cls, url):
+        return re.findall(r'archive.org\/(?:(?:web/)?)(?:(?:[0-9]{4})?(?:\*/)?)(.*)', url)[0]
 
     def snapshots(self, url_for_query):
         snapshots = set()
@@ -55,16 +64,41 @@ class WaybackMachineAPI(object):
         return snapshots
 
 
-def get_imgs(url, session, cw):
-    print_ = get_print(cw)
+def default(img):
+    if img.has_attr('src'):
+        return True
+    return False
+
+
+def twitter(img):
+    if default(img) and '/media/' in img['src']:
+        return True
+    return False
+
+
+filter_ = [
+    default,
+    twitter,
+]
+
+
+def get_filter_code(query):
+    if 'twitter.com' in query.lower():
+        return 1
+    return 0
+
+
+def get_imgs(query, session, cw):
     imgs = []
-    url_for_query = re.findall(r'archive.org\/(?:(?:web/)?)(?:(?:[0-9]{4})?(?:\*/)?)(.*)', url)
-    for snapshot in WaybackMachineAPI(session, cw).snapshots(url_for_query):
-        #html = downloader.read_html(url, session=session)
-        html = session.get(snapshot).text
-        print_(html)
-        soup = Soup(html)
-        for image in soup.find_all('img'):
-            if image.has_attr('src'):
-                imgs.append(image['src'])
+    img_elements = None
+    snapshots = WaybackMachineAPI(session, cw).snapshots(query)
+    filter_code = get_filter_code(query)
+    for url in snapshots:
+        soup = downloader.read_soup(url)
+        img_elements = soup.find_all('img')
+        for img in img_elements:
+            if filter_[filter_code](img):
+                imgs.append(img['src'])
     return imgs
+
+
