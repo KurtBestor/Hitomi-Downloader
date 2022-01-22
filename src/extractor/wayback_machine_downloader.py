156545e9363731a87d9d81a6ae93ace198c755f7
bog_4t <97702184+bog4t@users.noreply.github.com>
2022-01-22 04:34:03 -0500

    wayback_machine_downloader.py: fully implemented

--- src/extractor/wayback_machine_downloader.py
+++ src/extractor/wayback_machine_downloader.py
@@ -1,110 +1,194 @@
-# coding: utf8
+# coding: cp1252
 # title: Wayback Machine Downloader
-# comment: https://archive.org
 # author: bog_4t
+import concurrent.futures
 
-import downloader, os
-from utils import Downloader, Session, get_print, clean_title, update_url_query
-from ratelimit import limits, sleep_and_retry
+import downloader, json, os
+from utils import Downloader, Session, clean_title, get_print, update_url_query, print_error
 import ree as re
-import json
-import datetime
-import errors
-from error_printer import print_error
 from hashlib import md5
+from ratelimit import limits, sleep_and_retry
 
 
 @Downloader.register
 class Downloader_wayback_machine(Downloader):
     type = 'waybackmachine'
     URLS = ['archive.org', 'web.archive.org']
-    icon = 'https://archive.org/services/img/publicsafetycode'
+    icon = 'https://archive.org/offshoot_assets/favicon.ico'
     display_name = 'Wayback Machine'
 
     def init(self):
-        self.session = Session()
+        self.print = get_print(self.cw)
 
     def read(self):
-        url_for_query = WaybackMachineAPI.get_query(self.url)
-        snapshots = WaybackMachineAPI(self.session, self.cw).snapshots(url_for_query)
-        domain_code = get_domain_code(url_for_query)
-        self.urls.extend(get_imgs(snapshots, domain_code, self.cw))
-        name = self.url
-        for esc in ['?', '#']:
-            name = name.split(esc)[0]
-        name = os.path.basename(name.strip('/'))
-        tail = ' ({})'.format(md5(self.url.encode('utf8')).hexdigest()[:8])
-        self.title = clean_title(name, n=-len(tail)) + tail
+        e = Extractor(self.url, self.cw)
+        self.url = f'https://web.archive.org/cdx/search/cdx?url={e.url}'
+
+        self.title = e.title
+        self.urls.extend(get_imgs(self.url, e, self.dir, self.session, self.cw))
+        self.title = e.title
 
 
 class WaybackMachineAPI(object):
     def __init__(self, session, cw=None):
-        self.url_api = 'https://web.archive.org/cdx/search/cdx'
         self.session = session
         self.cw = cw
         self.params = {
-            'status code': '200',
-            'filter': 'mimetype:text/html',
-            'fastLatest': 'true',
-            'collapse': 'urlkey',
+            'output': 'json',
             'fl': 'timestamp,original',
-            'output': 'json'
+            'filter': 'mimetype:text/html',
+            'statuscode': '200',
+            'collapse': 'urlkey',
+            'limit': '5'
         }
 
     @sleep_and_retry
-    @limits(1, 2)
-    def call(self, url_for_query):
-        params = self.params
-        params['url'] = url_for_query
-        url = update_url_query(self.url_api, params)
+    @limits(5, 1)
+    def call(self, url):
+        get_print(self.cw)(url)
+        url = update_url_query(url, self.params)
         return downloader.read_json(url, session=self.session)
 
-    @classmethod
-    def get_query(cls, url):
-        return re.findall(r'archive.org\/(?:(?:web/)?)(?:(?:[0-9]+)?(?:\*)?/?)(.*)', url)[0]
-
-    def snapshots(self, url_for_query):
-        data = self.call(url_for_query)
-        if not data:
-            raise Exception('No archives found')
-        return data[1:]
+    def snapshots(self, url):
+        data = self.call(url)
+        return data[1:] or None
 
 
-def get_imgs(snapshots, domain_code, cw=None):
+class Extractor(object):
+    domains = [
+        'twitter.com'
+    ]
+
+    def __init__(self, url, cw=None):
+        self.cw = cw
+        self.print = get_print(cw)
+        self.url = re.findall(r'archive.[^/]+/(?:cdx/search/cdx\?url=|(?:web/)?(?:[^/]+/))([^&]+)', url.lower())[0].split('&')[0]
+        self.md5 = md5(self.url.encode('utf8')).hexdigest()[:8]
+        self.mode = self.__get_mode()
+        self.title = self.__get_title()
+
+    def __get_mode(self):
+        for mode in [mode for mode, domain in enumerate(self.domains, start=1) if domain in self.url]:
+            return mode
+        return 0
+
+    def __get_title(self):
+        def default():
+            tail = f' ({self.md5})'
+            return clean_title(os.path.basename(self.url.strip('*').strip('/')), n=-len(tail)) + tail
+
+        def twitter():
+            return re.findall('twitter.[^/]+/([^/?]+)', self.url)[0]
+
+        return [
+            default,
+            twitter
+        ][self.mode]()
+
+    def get_imgs_soup(self, soup, snapshot):
+        base_url = 'https://web.archive.org/web/{}im_/{}'
+
+        if not soup:
+            return []
+
+        def default():
+            return [base_url.format(snapshot[0], img['src']) for img in soup.find_all('img', src=True)]
+
+        def twitter():
+            return [base_url.format(snapshot[0], div['data-image-url']) for div in soup.find_all('div', {'data-image-url': True})]
+
+        return [
+            default,
+            twitter
+        ][self.mode]()
+
+
+class Bitmap(object):
+    bitmask = [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80]
+
+    def __init__(self, size=0, cw=None):
+        self.cw = cw
+        self.bitmap = bytearray([False] * ((size + 7) // 8))
+
+    def set(self, index):
+        self.bitmap[index // 8] |= self.bitmask[index % 8]
+
+    def unset(self, index):
+        self.bitmap[index // 8] &= ~self.bitmask[index % 8]
+
+    def get(self, index):
+        return (self.bitmap[index // 8] & (self.bitmask[index % 8])) != 0
+
+    def save(self, path):
+        with open(path, 'wb') as file:
+            file.seek(0)
+            file.write(self.bitmap)
+
+    def load(self, size, path):
+        with open(path, 'rb') as file:
+            self.bitmap = bytearray(file.read((size + 7)//8))
+        return self
+
+    def update(self, id_, path):
+        self.set(id_)
+        self.save(path)
+
+    def print(self, size):
+        get_print(self.cw)([str(1) if self.get(i) else str(0) for i in range(0, size)])
+
+
+def get_imgs(url, extractor, directory, session=Session(), cw=None):
+    e = extractor
     print_ = get_print(cw)
-    imgs = []
-    for snapshot in snapshots:
-        try:
-            soup = downloader.read_soup('https://web.archive.org/web/{}id_/{}'.format(snapshot[0], snapshot[1]))
-        except Exception as e:
-            print_(print_error(e)[0])
-            continue
-        for url in img_extractor[domain_code](soup, print_):
-            imgs.append('https://web.archive.org/web/{}im_/{}'.format(snapshot[0], url))
-    return imgs
 
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-def get_domain_code(query):
-    if 'twitter.com' in query.lower():
-        return 1
-    return 0
+    urls_path = os.path.join(directory, '{}.urls'.format(e.md5))
+    bitmap_path = os.path.join(directory, '{}.bitmap'.format(e.md5))
+    count_path = os.path.join(directory, '{}.count'.format(e.md5))
 
+    for path in [urls_path, bitmap_path, count_path]:
+        if not os.path.exists(path):
+            open(path, 'x').close()
 
-def default(soup):
-    urls = []
-    for img in soup.find_all('img', src=True):
-        urls.append(img['src'])
+    with open(count_path) as file:
+        num_complete = (lambda x: int(x) if x else 0)(file.read())
+
+    snapshots = WaybackMachineAPI(session, cw).snapshots(url)
+    bitmap = Bitmap(cw=cw).load(len(snapshots), bitmap_path) if num_complete else Bitmap(len(snapshots), cw=cw)
+
+    def get_imgs_snapshot(id_, snapshot):
+        @sleep_and_retry
+        @limits(5, 1)
+        def get_soup():
+            try:
+                return downloader.read_soup(f'https://web.archive.org/web/{snapshot[0]}id_/{snapshot[1]}')
+            except Exception as exception:
+                print_(print_error(exception)[0])
+                return None
+
+        return id_, e.get_imgs_soup(get_soup(), snapshot)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        futures = [executor.submit(get_imgs_snapshot, id_, snapshot) for id_, snapshot in enumerate(snapshots) if not bitmap.get(id_)]
+
+    with open(urls_path, 'a') as urls_file:
+        for future in concurrent.futures.as_completed(futures):
+            id_, urls = future.result()
+            urls_file.writelines([f'{url}\n' for url in urls])
+            bitmap.update(id_, bitmap_path)
+            num_complete += 1
+            str_num_complete = str(num_complete)
+            with open(count_path, 'w') as count_file:
+                count_file.write(str_num_complete)
+            cw.setTitle('{} - {}'.format(e.title, str_num_complete))
+
+    with open(urls_path) as file:
+        urls = [re.findall(r'^\S+$', lines)[0] for lines in file.readlines()]
+
+    os.remove(urls_path)
+    os.remove(bitmap_path)
+    os.remove(count_path)
+
     return urls
-
-
-def twitter(soup, print_):
-    urls = []
-    for div in soup.find_all('div', {'data-image-url': True}):
-        urls.append(div['data-image-url'])
-    return urls
-
-
-img_extractor = [
-    default,
-    twitter,
-]
